416dd0056f20a7b7661a7cd557466f946345a017
Miguel <miguelgarciarodriguez006@gmail.com>
2026-09-26 00:03:12 +0200

    Fix error TypeScript en BooksPage

--- frontend/app/book/page.tsx
+++ frontend/app/book/page.tsx
@@ -5,7 +5,7 @@ export default function BooksPage() {
     <main className="min-h-screen bg-[#14181c] text-white font-sans">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         <h1 className="text-2xl font-extrabold mb-6">Books</h1>
-        <BookSearchBox />
+        <BookSearchBox añoActual={2024} librosDelAño={[]}/>
       </div>
     </main>
   );
